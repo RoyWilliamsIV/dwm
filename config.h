c4c2bf253c514c209e83f649ccff903636be82b6
Roy Williams IV <r.e.williamsiv@pm.me>
2019-02-11 16:43:35 -0600

    4 tag test

--- config.h
+++ config.h
@@ -87,12 +87,6 @@ static Key keys[] = {
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
 	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	TAGKEYS(			XK_0,			   9)
 	{ MODKEY|ShiftMask,             XK_e,      quit,           {0} },
 };
 
