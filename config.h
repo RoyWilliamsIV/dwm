d0a59e6921b7730c191f303117717f6cc03247cb
Roy Williams IV <r.e.williamsiv@pm.me>
2019-03-06 12:45:33 -0600

    added networkmanager keybinding

--- config.h
+++ config.h
@@ -57,10 +57,12 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *netcmd[]  = { "networkmanager_dmenu", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+        { MODKEY|ShiftMask,             XK_n,      spawn,          {.v = netcmd } },
 	{ MODKEY|ShiftMask,	        XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
