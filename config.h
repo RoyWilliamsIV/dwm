be1fe7bd4e6a2621a186fdeb2f9375554fd09344
Roy Williams IV <r.e.williamsiv@pm.me>
2019-06-21 00:35:23 -0500

    update

--- config.h
+++ config.h
@@ -19,7 +19,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9"};
 
 static const Rule rules[] = { 
 	/* xprop(1):
@@ -57,13 +57,19 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *termalsacmd[]  = { "st", "alsamixer" };
 static const char *netcmd[]  = { "networkmanager_dmenu", NULL };
+static const char *brightup[] = { "light", "-A 20"};
+static const char *brightdown[] = { "light", "-U 20"};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
         { MODKEY|ShiftMask,             XK_n,      spawn,          {.v = netcmd } },
 	{ MODKEY|ShiftMask,	        XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,		XK_Home,   spawn,	   {.v = brightup } },
+	{ MODKEY|ShiftMask,		XK_End,    spawn,	   {.v = brightdown } },
+	{ MODKEY|ShiftMask,	        XK_m,      spawn,          {.v = termalsacmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -89,6 +95,11 @@ static Key keys[] = {
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
 	TAGKEYS(                        XK_4,                      3)
+	TAGKEYS(                        XK_5,                      4)
+	TAGKEYS(                        XK_6,                      5)
+	TAGKEYS(                        XK_7,                      6)
+	TAGKEYS(                        XK_8,                      7)
+	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_e,      quit,           {0} },
 };
 
